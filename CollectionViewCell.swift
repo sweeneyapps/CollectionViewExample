c14ad2b7a4a73147bbb638e0ea106c6d94c1df25
Paul <paul@sweeneyapps.com>
2015-06-27 19:40:44 -0700

    updated example
    add custom cell with colors and counter.

---
+++ CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewExample
+//
+//  Created by Paul Sweeney Jr on 6/27/15.
+//  Copyright (c) 2015 Paul Sweeney Jr. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var textLabel: UILabel!
+}
